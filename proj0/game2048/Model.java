030f309cdf3251d02789cb980faacb0c35e12303
Frank Shang <ftshang@berkeley.edu>
2024-07-21 10:50:19 -0700

    Implemented maxTileExists method.

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -153,7 +153,14 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        for (int row = 0; row < b.size(); row += 1) {
+            for (int col = 0; col < b.size(); col += 1) {
+                Tile tile = b.tile(row, col);
+                if (tile != null && tile.value() == MAX_PIECE) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
